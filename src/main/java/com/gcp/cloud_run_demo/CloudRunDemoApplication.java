ace7364ee7e7d61bbea0f7ca8fb83f68125362d4
Bharath Kumaar <bkumar@e2clouds.com>
2024-09-19 16:05:59 +0530

    First commit

---
+++ src/main/java/com/gcp/cloud_run_demo/CloudRunDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gcp.cloud_run_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudRunDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudRunDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gcp/cloud_run_demo/CloudRunDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcp.cloud_run_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudRunDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
